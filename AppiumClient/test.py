0d01a0c8a4aefdccbcf216ad40916f70cad8b83e
Confiot <Confiot>
2023-11-12 08:55:47 +0000

    update

--- AppiumClient/test.py
+++ AppiumClient/test.py
@@ -19,7 +19,7 @@ appium_server_url = 'http://localhost:4723'
 class TestAppium(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.driver = webdriver.Remote(appium_server_url, options=capabilities)
+        self.driver = webdriver.Remote(appium_server_url, capabilities)
 
     def tearDown(self) -> None:
         if self.driver:
@@ -31,4 +31,4 @@ class TestAppium(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    driver = webdriver.Remote(appium_server_url, options=capabilities)
+    unittest.main()
